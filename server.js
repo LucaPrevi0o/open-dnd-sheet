5634a44d4a4fbc1cabdedfaad8bbbf39ebb66408
LucaPrevi0o <luca01.previati@edu.unife.it>
2026-09-18 12:46:22 +0200

    feat: update global server to render landing page

--- server.js
+++ server.js
@@ -8,13 +8,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON data
 app.use(express.json());
 
-// Basic route to check if server is running
-app.get('/', (req, res) => {
-    res.send('Open D&D Sheet is running!');
-});
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
